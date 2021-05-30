e022702cf853ef0e41d46bd3b5c32053e3f07048
Thrichen Khentse <tkhentshey23@gmail.com>
2021-05-31 01:03:40 +0600

    update

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  ID: number;
+  title: string;
+  project: number;
+  Status: string;
+ 
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {ID: 1, name: 'Pema Wangchuk',title: 'UI', project: 3, Status: 'Active'},
+  {ID: 2, name: 'Tashi Tobgyel',title: 'UX', project: 5, Status: 'Active'},
+  {ID: 3, name: 'Sonam Choden',title: 'BA', project: 2, Status: 'Active'},
+  {ID: 4, name: 'Chimi Yangden',title: 'UX', project: 1, Status: 'Active'},
+  {ID: 5, name: 'Tshering Choki',title: 'UI', project: 1, Status: 'Inactive'},
+  {ID: 6, name: 'Tashi Wangdi',title: 'BE',project: 1, Status: 'Active'},
+  {ID: 7, name: 'Pema Yangki',title: 'UI', project: 4, Status: 'Active'},
+  {ID: 8, name: 'Dorji Wangchuk',title: 'UX', project: 5, Status: 'Active'},
+  {ID: 9, name: 'Thirchen Khentse', title: 'FE',project: 5, Status: 'Inactive'},
+  {ID: 10, name: 'Dawa', project: 2, title: 'FE',Status: 'Active'},
 ];
 @Component({
   selector: 'app-employee',
@@ -24,7 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['ID', 'name', 'project', 'title', 'Status'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  status: string;
+  developer: number;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Name of Project', status: 'complete', developer: 1},
+  {position: 2, name: 'Name of Project', status: 'complete', developer: 1},
+  {position: 3, name: 'Name of Project', status: 'process', developer: 4},
+  {position: 4, name: 'Name of Project', status: 'process', developer: 4},
+  {position: 5, name: 'Name of Project', status: 'inactive', developer: 0},
+  {position: 6, name: 'Name of Project', status: 'inactive', developer: 0},
+  {position: 7, name: 'Name of Project', status: 'process', developer: 5},
+  {position: 8, name: 'Name of Project', status: 'complete', developer: 2},
+  {position: 9, name: 'Name of Project', status: 'complete', developer: 2},
+  {position: 10, name: 'Name of Project', status: 'complete', developer: 2},
+];
 
 @Component({
   selector: 'app-status',
@@ -6,7 +24,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status.component.scss']
 })
 export class StatusComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'status', 'developer'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
